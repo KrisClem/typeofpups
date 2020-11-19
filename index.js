925f3ea666c99c671bd5c96d005c44104ed2de06
KrisClem <71985892+KrisClem@users.noreply.github.com>
2020-11-19 10:57:39 -0800

    finished app

--- index.js
+++ index.js
@@ -3,19 +3,31 @@ function getDogImage(breed) {
     const apiLinkString = `https://dog.ceo/api/breed/${breed}/images/random`
     
     fetch(apiLinkString)
+        .then(response => fetchStatusHandler(response))
         .then(response => response.json())
         .then(responseJson => showPups(responseJson.message))
-        .catch(error => alert('Something went wrong. Try again later.'));
+        .catch((error) => console.error('Error:', error));
 }
 
-function showPups(responseJson) {
+function showPups(responseJson) { 
     $('.results-img').replaceWith(
-        `<img src=${responseJson}>`
+        `<img class="results-img" src=${responseJson}>`
     )
 
     $('.results').removeClass('hidden');
 }
 
+function fetchStatusHandler(response) {
+  if (response.status === 200) {
+    return response;
+  } else {
+    $('.results').replaceWith(
+        `<h2>Oh no, there are no pups of that breed.</h2>
+        <h3>Please try a different breed</h3>`
+    )
+    $('.results').removeClass('hidden');
+  }
+}
 
 function submitBreed() {
     $('form').on('click', '.submit-button', event => {
